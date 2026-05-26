0ff96369b53981efbf195bb11e68c5b6eb66e0b5
Ariadna Alemán <ariadnarosarioshaiela@gmail.com>
2026-05-26 17:03:09 -0300

    Análisis Avanzado de Datos

--- importandoCsv.py
+++ importandoCsv.py
@@ -13,8 +13,18 @@ print(df.head())
 
 #print(resultado)
 
-total_visita = df['VISITA_ARTE'].count()
-print(f"Total de registros: {total_visita}")
+#total_visita = df['VISITA_ARTE'].count()
+#print(f"Total de registros: {total_visita}")
 
-total_InteresE = df['INTERES_ECONOMIA'].sum()
-print(f"Total de intereses Economia: {total_InteresE}")
+#total_InteresE = df['INTERES_ECONOMIA'].sum()
+#print(f"Total de intereses Economia: {total_InteresE}")
+
+print("--- Análisis Avanzado de Datos ---")
+
+filtro_avanzado = df['APRECIO_ARTISTAS'].str.startswith("NA", na=False)
+df_filtrado = df[filtro_avanzado]
+
+sumo_desEdu = df_filtrado['DESTAQUE_EDUCACION'].sum()
+print("--- Reporte ---")
+print(f"Total: {sumo_desEdu} destaque.\n")
+filtro_numero = df['DESTAQUE_EDUCACION'] > 200
