3d4f74c08b7afbe21dac1b1fa57f64f94cc10236
Ariadna Alemán <ariadnarosarioshaiela@gmail.com>
2026-05-26 15:56:58 -0300

    archivo gitignore

--- importandoCsv.py
+++ importandoCsv.py
@@ -4,3 +4,17 @@ import pandas as pd
 df=pd.read_csv("dataset_percepcion.csv")
 
 print("OKEY! Archivo cargado correctamente")
+
+#Mostrando las primeras filas del dataframe
+print(df.head())
+
+#Mostrar resultado que año sea igual a 2022
+#resultado = df[df['ANIO'] == 2022]
+
+#print(resultado)
+
+total_visita = df['VISITA_ARTE'].count()
+print(f"Total de registros: {total_visita}")
+
+total_InteresE = df['INTERES_ECONOMIA'].sum()
+print(f"Total de intereses Economia: {total_InteresE}")
