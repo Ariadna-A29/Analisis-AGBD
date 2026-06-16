ce60834ecb37d7448fddafad4395c26a1f88b7f8
Ariadna Alemán <ariadnarosarioshaiela@gmail.com>
2026-06-16 16:11:44 -0300

    Arreglar variables x e y

--- importandoCsv.py
+++ importandoCsv.py
@@ -1,30 +1,55 @@
+#Importamos las librerías necesarias
 import pandas as pd
+import matplotlib as plt
+import seaborn as sns
 
+#Cargo de datos
 #Solo ponemos el nombre exacto del archivo entre comillas
-df=pd.read_csv("dataset_percepcion.csv")
+df = pd.read_csv("dataset_percepcion.csv")
 
 print("OKEY! Archivo cargado correctamente")
 
-#Mostrando las primeras filas del dataframe
-print(df.head())
+#Logica de filtrado
+filtro_avanzado = df["DESTAQUE_SALUD"].str.starswith("1", na= False)
+df_filtrado = df(filtro_avanzado)
+suma_dinero = df_filtrado["INFORMACION_MEDICINA_SALUD"].sum()
 
-#Mostrar resultado que año sea igual a 2022
-#resultado = df[df['ANIO'] == 2022]
+print("----- Reporte Automatizado -----")
+print(f"Monto finalizado: {suma_dinero:.2f} puntos")
 
-#print(resultado)
+#Condicional
+if default_limite_alto := (suma_dinero > 500):
+    print("Alerta! La información supera el limite.")
+    print("Requiere revisión inmediata")
+elif suma_dinero < 100:
+    print("Aviso: mercado moderado/alto")
+    print("Monitorear comportamiento prox trim")
+else:
+    print("Mercado estable, sin alertas por el momento.")
 
-#total_visita = df['VISITA_ARTE'].count()
-#print(f"Total de registros: {total_visita}")
+#-------------------------------------
+#Gráfico de barras usando toda DF
+#-------------------------------------
+print("\n[Generando GRAFICO de Barras]")
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(10,6))
+sns.barplot(
+    data=df,
+    x="DESTAQUE_SALUD",
+    y="INFORMACION_MEDICINA_SALUD",
+    estimator=sum,
+    errorbar=None,
+    palette="viridis",
+)
+plt.title("Comparativa de información medicina por región", fontsize=14)
+plt.xticks(rotation=20)
 
-#total_InteresE = df['INTERES_ECONOMIA'].sum()
-#print(f"Total de intereses Economia: {total_InteresE}")
+plt.savefig("grafico_barra.png", dpi=300)
+plt.close()
 
-print("--- Análisis Avanzado de Datos ---")
+print("\n¡Hecho! Los gráficos se guardaron correctamente en tu carpeta")
 
-filtro_avanzado = df['APRECIO_ARTISTAS'].str.startswith("NA", na=False)
-df_filtrado = df[filtro_avanzado]
+plt.tight_layout()
+plt.show()
 
-sumo_desEdu = df_filtrado['DESTAQUE_EDUCACION'].sum()
-print("--- Reporte ---")
-print(f"Total: {sumo_desEdu} destaque.\n")
-filtro_numero = df['DESTAQUE_EDUCACION'] > 200
+
